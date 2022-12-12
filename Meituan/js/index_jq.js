b277aea9bc1d991649540c4b6fc8bc3179109e05
liusxs <1694181697@qq.com>
2022-12-12 10:37:08 +0800

    更新

---
+++ Meituan/js/index_jq.js
@@ -0,0 +1,39 @@
+$(function() {
+
+    var sheng = areaData.sheng;
+    var $sheng = $('#sheng');
+    var $shi = $('#shi');
+    var $xian = $('#xian');
+    var shiIndex = 0;
+    for(var i = 0; i < sheng.length; i++) {
+        var $option = $('<option value=' + (i + 1) + '>' + sheng[i] + '</option>');
+        $sheng.append($option);
+    }
+    $sheng.change(function() {
+        shiIndex = this.selectedIndex - 1;
+        if(shiIndex < 0) {
+
+        } else {
+            var shi = areaData.shi['a_' + shiIndex];
+            $shi.html('<option value="0">市</option>');
+            $xian.html('<option value="0">县</option>');
+            for(var i = 0; i < shi.length; i++) {
+                var $option = $('<option value=' + (i + 1) + '>' + shi[i] + '</option>');
+                $shi.append($option);
+            }
+        }
+    });
+    $shi.change(function() {
+        var index = this.selectedIndex - 1;
+        if(index < 0) {
+
+        } else {
+            var xian = areaData.xian['a_' + shiIndex + '_' + index];
+            $xian.html('<option value="0">县</option>');
+            for(var i = 0; i < xian.length; i++) {
+                var $option = $('<option value=' + (i + 1) + '>' + xian[i] + '</option>');
+                $xian.append($option);
+            }
+        }
+    });
+});
