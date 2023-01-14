249591e38755847359d7e664c46b018d6fed4ce1
liusxs <1694181697@qq.com>
2023-01-14 21:00:37 +0800

    Create snake.js

---
+++ GluttonousSnake/snake.js
@@ -0,0 +1,15 @@
+// 蛇对象
+class Snake{
+    constructor(select){
+        this.map=document.querySelector(select)
+        // 蛇的运动方向
+        this.direction="right"
+        // 蛇的数组(把蛇的头和身体都会储存到数组中，头从数组的第0位开始)
+        this.snakelist=[]
+    }
+    // 创建蛇头的函数
+    createHead(){
+        // 拿到蛇头
+        
+    }
+}
