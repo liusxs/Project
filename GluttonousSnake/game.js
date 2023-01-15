b194180293168d099f018619b0e5dc0e48ecadd3
liusxs <1694181697@qq.com>
2023-01-15 22:41:37 +0800

    Update game.js

--- GluttonousSnake/game.js
+++ GluttonousSnake/game.js
@@ -1,15 +1,19 @@
 // 设计游戏类
 class Game{
-    constructor(select){
+    constructor(select,scoreEle){
         // 地图
         this.map=document.querySelector(select)
+        // 计分板
+        this.scoreEle=document.querySelector(scoreEle)
         // 食物
         this.food = new Food(select)
         // 蛇
         this.snake = new Snake(select)
         // 定义计时器
         this.timer = 0
-        this.start()
+        // this.start()
+        // 得分
+        this.cunt=0
     }
     // 定义游戏开始的方法
     start(){
@@ -22,10 +26,13 @@ class Game{
                 this.snake.createHead()
                 // 食物的位置更新
                 this.food.foodPos()
+                // 调用得分增加更新分数
+                this.scorechange()
             }
             // 判断蛇是否死亡
             if(this.snake.isDie()){
                 clearInterval(this.timer)
+                this.gameover()
             }
         }, 500);
     }
@@ -40,5 +47,14 @@ class Game{
     // 改变方向的方法
     change(type){
         this.snake.direction = type
+    }
+    // 得分增加
+    scorechange(){
+        this.cunt++
+        this.scoreEle.innerText=this.cunt
+    }
+    // 游戏结束的方法
+    gameover(){
+        alert("游戏结束!")
     }
 }
