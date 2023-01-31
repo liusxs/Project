09ac64332f41421d528f8972f524836226211fc0
liusxs <1694181697@qq.com>
2023-02-01 01:18:37 +0800

    update

--- Manage/src/main.js
+++ Manage/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
+// import ElementUI from 'element-ui';
+import {Row, Button} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-// 全局注入
-Vue.use(ElementUI);
+// 全局引入
+// Vue.use(ElementUI)
+
+// 按需引入
+Vue.use(Row)
+Vue.use(Button)
 
 new Vue({
   el: '#app',
