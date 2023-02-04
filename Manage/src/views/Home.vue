d05d8594c421cca58944c9d6bd3841b6e30b32b2
liusxs <1694181697@qq.com>
2023-02-04 22:04:21 +0800

    Update Home.vue

--- Manage/src/views/Home.vue
+++ Manage/src/views/Home.vue
@@ -14,16 +14,121 @@
                     <p>上次登录地点:<span>火星</span></p>
                 </div>
             </el-card>
+            <el-card style="margin-top: 10px;height: 350px;">
+                <el-table :data="tableData" style="width: 100%">
+                    <!-- <el-table-column prop="name" label="课程">
+                    </el-table-column>
+                    <el-table-column prop="todayBuy" label="今日购买">
+                    </el-table-column>
+                    <el-table-column prop="monthBuy" label="本月购买">
+                    </el-table-column>
+                    <el-table-column prop="totalBuy" label="总购买">
+                    </el-table-column> -->
+                    <el-table-column v-for="(val,key) in tableLabel" :prop="key" :label="val">
+                    </el-table-column>
+                </el-table>
+            </el-card>
         </el-col>
         <el-col :span="16">
-            <div class="grid-content bg-purple-light"></div>
+            <div class="num">
+                <el-card v-for="item in countData" :key="item.name" :body-style="{display:'flex', padding:0}">
+                    <i class="icon" :class="`el-icon-${item.icon}`" :style="{background: item.color}"></i>
+                    <div class="detail">
+                        <p class="price">￥{{item.value}}</p>
+                        <p class="desc">{{item.name}}</p>
+                    </div>
+                </el-card>
+            </div>
         </el-col>
     </el-row>
 </template>
 <script>
 export default {
     data() {
-        return {}
+        return {
+            tableData:[
+                {
+                    name:'oppo',
+                    todayBuy:'100',
+                    monthBuy:'300',
+                    totalBuy:'800'
+                },
+                {
+                    name:'vivo',
+                    todayBuy:'100',
+                    monthBuy:'300',
+                    totalBuy:'800'
+                },
+                {
+                    name:'魅族',
+                    todayBuy:'100',
+                    monthBuy:'300',
+                    totalBuy:'800'
+                },
+                {
+                    name:'华为',
+                    todayBuy:'100',
+                    monthBuy:'300',
+                    totalBuy:'800'
+                },
+                {
+                    name:'小米',
+                    todayBuy:'100',
+                    monthBuy:'300',
+                    totalBuy:'800'
+                },
+                {
+                    name:'菠萝',
+                    todayBuy:'100',
+                    monthBuy:'300',
+                    totalBuy:'800'
+                },
+            ],
+            tableLabel:{
+                name :'课程',
+                todayBuy:'今日购买',
+                monthBuy:'本月购买',
+                totalBuy:'总购买'
+            },
+            countData:[
+                {
+                    name:'今日支付订单',
+                    value:'1234',
+                    icon:'success',
+                    color:'#2ec7c9',
+                },
+                {
+                    name:'今日收藏订单',
+                    value:'210',
+                    icon:'star-on',
+                    color:'#ffb980',
+                },
+                {
+                    name:'今日未支付订单',
+                    value:'1234',
+                    icon:'s-goods',
+                    color:'#5ab1ef',
+                },
+                {
+                    name:'本月支付订单',
+                    value:'1234',
+                    icon:'success',
+                    color:'#2ec7c9',
+                },
+                {
+                    name:'本月收藏订单',
+                    value:'210',
+                    icon:'star-on',
+                    color:'#ffb980',
+                },
+                {
+                    name:'本月未支付订单',
+                    value:'1234',
+                    icon:'s-goods',
+                    color:'#5ab1ef',
+                },
+            ],
+        }
     }
 }
 </script>
@@ -64,6 +169,40 @@ export default {
             color: #666666;
             margin-left: 60px;
         }
+    }
+}
+.num{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    .icon{
+        width: 80px;
+        height: 80px;
+        font-size: 30px;
+        text-align: center;
+        line-height: 80px;
+        color: #fff;
+    }
+    .detail{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-left: 15px;
+        .price{
+            font-size: 30px;
+            margin-bottom: 10px;
+            line-height: 30px;
+            height: 30px;
+        }
+        .desc{
+            font-size: 14px;
+            color: #999;
+            text-align: center;
+        }
+    }
+    .el-card{
+        width: 30%;
+        margin-bottom: 20px;
     }
 }
 </style>
