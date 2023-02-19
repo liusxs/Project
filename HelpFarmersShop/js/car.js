35a9b793c4ee9e456ccbc87b1416e001075798b4
liusxs <1694181697@qq.com>
2023-02-19 09:09:03 +0800

    update

--- HelpFarmersShop/js/car.js
+++ HelpFarmersShop/js/car.js
@@ -1,16 +1,16 @@
-$(function(){
+$(function () {
     // 设置商品数量的上下限额
-    var min_num=1;
-    var max_num=10;
-    $('.checkall').change(function(){
-        var isChecked=$(this).prop('checked');
-        $('.j-checkbox').prop('checked',isChecked)
-        $('.j-checkbox').prop('checked',$(this).prop('checked'));
-        $('.checkall,j-checkbox').prop('checked',$(this).prop('checked'));
+    var min_num = 1;
+    var max_num = 10;
+    $('.checkall').change(function () {
+        var isChecked = $(this).prop('checked');
+        $('.j-checkbox').prop('checked', isChecked)
+        $('.j-checkbox').prop('checked', $(this).prop('checked'));
+        $('.checkall,j-checkbox').prop('checked', $(this).prop('checked'));
         // 全选中的商品增加背景色
-        if($(this).prop('checked')){
+        if ($(this).prop('checked')) {
             $('.cart-item').addClass('check-cart-item')
-        }else{
+        } else {
             $('.cart-item').removeClass('check-cart-item')
         }
         getSum();
@@ -30,60 +30,60 @@ $(function(){
     //     getSum();
     // })
     // 商品数量的增加与单价的增加
-    $('.increment').click(function(){
-        var n=$(this).siblings('.itxt').val();
-        if(n<max_num){
+    $('.increment').click(function () {
+        var n = $(this).siblings('.itxt').val();
+        if (n < max_num) {
             n++;
             $(this).siblings('.itxt').val(n);
-            var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
-            var sum=(p*n).toFixed(2);
-            $(this).parent().parent().siblings('.p-sum').html('￥'+sum);  
-            getSum();  
-        }else{
+            var p = $(this).parent().parent().siblings('.p-price').html().substr(1);
+            var sum = (p * n).toFixed(2);
+            $(this).parent().parent().siblings('.p-sum').html('￥' + sum);
+            getSum();
+        } else {
             alert('商品数量已达上限!');
             return true;
         }
     })
     // 商品数量的减少与单价的减少
-    $('.decrement').click(function(){
-        var m=$(this).siblings('.itxt').val();
-        if(m>min_num){
+    $('.decrement').click(function () {
+        var m = $(this).siblings('.itxt').val();
+        if (m > min_num) {
             m--;
             $(this).siblings('.itxt').val(m);
-            var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
-            var sum=(p*m).toFixed(2);
-            $(this).parent().parent().siblings('.p-sum').html('￥'+sum);
+            var p = $(this).parent().parent().siblings('.p-price').html().substr(1);
+            var sum = (p * m).toFixed(2);
+            $(this).parent().parent().siblings('.p-sum').html('￥' + sum);
             getSum();
-        }else{
-            alert('最少购买'+min_num+'件哦!');
+        } else {
+            alert('最少购买' + min_num + '件哦!');
             return false;
         }
     })
     // 文本框输入数值 小计随之改变 方法1
-    $('.itxt').change(function(){
-        var n=$(this).val();
-        var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
-        var sum=(p*n).toFixed(2);   
-        $(this).parent().parent().siblings('.p-sum').html('￥'+sum); 
+    $('.itxt').change(function () {
+        var n = $(this).val();
+        var p = $(this).parent().parent().siblings('.p-price').html().substr(1);
+        var sum = (p * n).toFixed(2);
+        $(this).parent().parent().siblings('.p-sum').html('￥' + sum);
         $(this).siblings('.itxt').val(n);
         getSum();
-        if(n<=min_num&&n>max_num){
-            var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
-            var sum=(p*n).toFixed(2);   
-            $(this).parent().parent().siblings('.p-sum').html('￥'+sum); 
+        if (n <= min_num && n > max_num) {
+            var p = $(this).parent().parent().siblings('.p-price').html().substr(1);
+            var sum = (p * n).toFixed(2);
+            $(this).parent().parent().siblings('.p-sum').html('￥' + sum);
         }
-        if(n<=min_num){
-            var n=$(this).val(min_num);
-            var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
-            var sum=(p*n).toFixed(2); 
-            $(this).parent().parent().siblings('.p-sum').html('￥'+p*min_num);   
+        if (n <= min_num) {
+            var n = $(this).val(min_num);
+            var p = $(this).parent().parent().siblings('.p-price').html().substr(1);
+            var sum = (p * n).toFixed(2);
+            $(this).parent().parent().siblings('.p-sum').html('￥' + p * min_num);
             alert('最少购买一件哦!');
         }
-        if(n>max_num){
-            var n=$(this).val(max_num);
-            var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
-            var sum=(p*n).toFixed(2);
-            $(this).parent().parent().siblings('.p-sum').html('￥'+p*max_num);
+        if (n > max_num) {
+            var n = $(this).val(max_num);
+            var p = $(this).parent().parent().siblings('.p-price').html().substr(1);
+            var sum = (p * n).toFixed(2);
+            $(this).parent().parent().siblings('.p-sum').html('￥' + p * max_num);
             alert('已达到商品上限!');
         }
         // $(this).parent().parent().siblings('.p-sum').html('￥'+sum);    
@@ -97,30 +97,30 @@ $(function(){
     // })
     // 计算商品总价值
     getSum();
-    function getSum(){
-        var count=0;
-        var price=0;
-        $('.itxt').each(function(index,element){
-            count+=parseInt($(element).val());
+    function getSum() {
+        var count = 0;
+        var price = 0;
+        $('.itxt').each(function (index, element) {
+            count += parseInt($(element).val());
         })
         $('.amount-sum em').text(count);
-        $('.p-sum').each(function(i,ele){
-            price+=parseFloat($(ele).text().substr(1));
+        $('.p-sum').each(function (i, ele) {
+            price += parseFloat($(ele).text().substr(1));
         })
-        $('.price-sum em').text('￥'+ price.toFixed(2));
+        $('.price-sum em').text('￥' + price.toFixed(2));
     }
     // 删除商品
-    $('.p-action a').click(function(){
+    $('.p-action a').click(function () {
         $(this).parent().parent('.cart-item').remove();
         getSum();
     })
     // 删除被选中的商品
-    $('.remove-batch').click(function(){
+    $('.remove-batch').click(function () {
         $('.j-checkbox:checked').parent().parent('.cart-item').remove();
         getSum();
     })
     // 删除所有的商品
-    $('.clear-all').click(function(){
+    $('.clear-all').click(function () {
         $('.cart-item').remove();
         getSum();
     })
--- HelpFarmersShop/js/index.js
+++ HelpFarmersShop/js/index.js
@@ -10,94 +10,94 @@
 //     clearTimeout(scrolldelay);
 // }
 // 获取元素
-var banner=document.querySelector('#banner-left');
-var bannerImg=document.querySelectorAll('#banner-left img');
-var bannerlist=document.querySelectorAll('.bannerNav li');
+var banner = document.querySelector('#banner-left');
+var bannerImg = document.querySelectorAll('#banner-left img');
+var bannerlist = document.querySelectorAll('.bannerNav li');
 // 创建变量
-var index=0;
+var index = 0;
 //  (1.鼠标不在轮播图区域的时候 轮播图正常播放)
-var t=setInterval(function(){
-    if(index==2){
-        index=0;
-    }else{
+var t = setInterval(function () {
+    if (index == 2) {
+        index = 0;
+    } else {
         index++;
     }
-    for(var n=0;n<=2;n++){
-        bannerImg[n].className='';
+    for (var n = 0; n <= 2; n++) {
+        bannerImg[n].className = '';
     }
-    bannerImg[index].className='show';
-    for(var n=0;n<=2;n++){
-        bannerlist[n].className='';
+    bannerImg[index].className = 'show';
+    for (var n = 0; n <= 2; n++) {
+        bannerlist[n].className = '';
     }
-    bannerlist[index].className='active';
+    bannerlist[index].className = 'active';
 }, 2000)
 //  (2.鼠标移入轮播图区域 轮播图播放暂停)
-banner.addEventListener('mouseenter',function(){
+banner.addEventListener('mouseenter', function () {
     clearInterval(t);
 })
 //  (3.鼠标移出轮播图区域 轮播图恢复播放)
-banner.addEventListener('mouseleave',function(){
-    t=setInterval(function(){
-        if(index==2){
-        index=0;
-    }else{
-        index++;
-    }
-    for(var n=0;n<=2;n++){
-        bannerImg[n].className='';
-    }
-    bannerImg[index].className='show';
-    for(var n=0;n<=2;n++){
-        bannerlist[n].className='';
-    }
-    bannerlist[index].className='active';
-    },2000)
+banner.addEventListener('mouseleave', function () {
+    t = setInterval(function () {
+        if (index == 2) {
+            index = 0;
+        } else {
+            index++;
+        }
+        for (var n = 0; n <= 2; n++) {
+            bannerImg[n].className = '';
+        }
+        bannerImg[index].className = 'show';
+        for (var n = 0; n <= 2; n++) {
+            bannerlist[n].className = '';
+        }
+        bannerlist[index].className = 'active';
+    }, 2000)
 })
-var bannerLeft=document.querySelector('#bannerLeft');
-var bannerRight=document.querySelector('#bannerRight');
-bannerLeft.addEventListener('click',function(){
-    if(index==0){
-        index=2;
-    }else{
+var bannerLeft = document.querySelector('#bannerLeft');
+var bannerRight = document.querySelector('#bannerRight');
+bannerLeft.addEventListener('click', function () {
+    if (index == 0) {
+        index = 2;
+    } else {
         index--;
     }
-    for(var n=0;n<=2;n++){
-        bannerImg[n].className='';
+    for (var n = 0; n <= 2; n++) {
+        bannerImg[n].className = '';
     }
-    bannerImg[index].className='show';
-    for(var n=0;n<=2;n++){
-        bannerlist[n].className='';
+    bannerImg[index].className = 'show';
+    for (var n = 0; n <= 2; n++) {
+        bannerlist[n].className = '';
     }
-    bannerlist[index].className='active';
+    bannerlist[index].className = 'active';
 })
-bannerRight.addEventListener('click',function(){
-    if(index==2){
-        index=0;
-    }else{
+bannerRight.addEventListener('click', function () {
+    if (index == 2) {
+        index = 0;
+    } else {
         index++;
     }
-    for(var n=0;n<=2;n++){
-        bannerImg[n].className='';
+    for (var n = 0; n <= 2; n++) {
+        bannerImg[n].className = '';
     }
-    bannerImg[index].className='show';
-    for(var n=0;n<=2;n++){
-        bannerlist[n].className='';
+    bannerImg[index].className = 'show';
+    for (var n = 0; n <= 2; n++) {
+        bannerlist[n].className = '';
     }
-    bannerlist[index].className='active';
+    bannerlist[index].className = 'active';
 })
 // 鼠标移入左下方导航（小圆点）切换到对应的图片
-for(var n=0;n<=2;n++){
-    bannerlist[n].index=n;
-    bannerlist[n].addEventListener('mouseenter',function(){
-        for(var n=0;n<=2;n++){
-            bannerImg[n].className='';
+for (var n = 0; n <= 2; n++) {
+    bannerlist[n].index = n;
+    bannerlist[n].addEventListener('mouseenter', function () {
+        for (var n = 0; n <= 2; n++) {
+            bannerImg[n].className = '';
         }
-        index=this.index;
-        for(var n=0;n<=2;n++){
-            bannerlist[n].className='';
+        index = this.index;
+        for (var n = 0; n <= 2; n++) {
+            bannerlist[n].className = '';
         }
-        bannerImg[index].className='show';
-        bannerlist[index].className='active';
+        bannerImg[index].className = 'show';
+        bannerlist[index].className = 'active';
     })
 }
 
@@ -105,42 +105,42 @@ for(var n=0;n<=2;n++){
 var scan = document.querySelector('.scan');
 var scan_img = document.querySelector('.scan_img');
 
-scan.onclick = function(){
-    if(scan_img.style.display == 'none'){
+scan.onclick = function () {
+    if (scan_img.style.display == 'none') {
         scan_img.style.display = 'block';
-    }else{
+    } else {
         scan_img.style.display = 'none';
     }
 }
 
 // 笔记：老式的纯JS滚动十分生硬，需要使用JQ达到更好的效果
-function pageScroll(){
+function pageScroll() {
     //把内容滚动指定的像素数（第一个参数是向右滚动的像素数，第二个参数是向下滚动的像素数）
-    window.scrollBy(0,-100);
+    window.scrollBy(0, -100);
     //延时递归调用，模拟滚动向上效果
-    scrolldelay = setTimeout('pageScroll()',100);
+    scrolldelay = setTimeout('pageScroll()', 100);
     //获取scrollTop值，声明了DTD的标准网页取document.documentElement.scrollTop，否则取document.body.scrollTop；因为二者只有一个会生效，另一个就恒为0，所以取和值可以得到网页的真正的scrollTop值
-    var sTop=document.documentElement.scrollTop+document.body.scrollTop;
+    var sTop = document.documentElement.scrollTop + document.body.scrollTop;
     //判断当页面到达顶部，取消延时代码（否则页面滚动到顶部会无法再向下正常浏览页面）
-    if(sTop==0) clearTimeout(scrolldelay);
+    if (sTop == 0) clearTimeout(scrolldelay);
 }
 
 window.onclick = function (ev) {
-    var clickTips = ["富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善"];
+    var clickTips = ["富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善"];
     var span = document.createElement('span');
     //随机显示文本处理 使用INNERTEXT显示 
-    span.innerText = clickTips[parseInt(Math.random()*clickTips.length)];
+    span.innerText = clickTips[parseInt(Math.random() * clickTips.length)];
 
-    span.setAttribute("style","z-index:999;position:absolute;left:"+ev.pageX+"px;top:"+(ev.pageY-20)+"px;animation-duration:1s;" +
-            "animation-fill-mode:both;animation-name:fadeOutUp;");
+    span.setAttribute("style", "z-index:999;position:absolute;left:" + ev.pageX + "px;top:" + (ev.pageY - 20) + "px;animation-duration:1s;" +
+        "animation-fill-mode:both;animation-name:fadeOutUp;");
     document.body.appendChild(span);
     //WebKit
-    if(typeof document.body.style.WebkitAnimation!="undefined"){
-        span.addEventListener("webkitAnimationEnd",function () {
+    if (typeof document.body.style.WebkitAnimation != "undefined") {
+        span.addEventListener("webkitAnimationEnd", function () {
             document.body.removeChild(span);
         });
-    }else{
-        span.addEventListener("animationend",function () {
+    } else {
+        span.addEventListener("animationend", function () {
             document.body.removeChild(span);
         });
     }
@@ -151,36 +151,36 @@ var btn_nav_li_list = document.getElementById("tab_nav").getElementsByTagName("l
 var btn_item_li_list = document.getElementById("tab_item").getElementsByTagName("li");
 
 // 初始化选项卡
-for(var itemi = 0; itemi < btn_item_li_list.length; itemi++) {
-	btn_item_li_list[itemi].style.display = "none";
+for (var itemi = 0; itemi < btn_item_li_list.length; itemi++) {
+    btn_item_li_list[itemi].style.display = "none";
 }
 // 默认第一个显示
 btn_item_li_list[0].style.display = "block";
 
 // 循环遍历选项卡导航菜单li
-for(var i = 0; i < btn_nav_li_list.length; i++) {
-	// 为所有li设置自定义属性index，用来检索选项卡项目
-	btn_nav_li_list[i].setAttribute("index", i);
+for (var i = 0; i < btn_nav_li_list.length; i++) {
+    // 为所有li设置自定义属性index，用来检索选项卡项目
+    btn_nav_li_list[i].setAttribute("index", i);
 
-	// 为所有li注册单击事件
-	btn_nav_li_list[i].onclick = function() {
-		// 先清除所有选项卡导航样式
-		for(var navi = 0; navi < btn_nav_li_list.length; navi++) {
-			btn_nav_li_list[navi].removeAttribute("class");
-		}
+    // 为所有li注册单击事件
+    btn_nav_li_list[i].onclick = function () {
+        // 先清除所有选项卡导航样式
+        for (var navi = 0; navi < btn_nav_li_list.length; navi++) {
+            btn_nav_li_list[navi].removeAttribute("class");
+        }
 
-		// 设置单击时候样式
-		this.className = "btn-active";
+        // 设置单击时候样式
+        this.className = "btn-active";
 
-		// 获取当前选项卡导航li的自定义属性index
-		var index = this.getAttribute("index");
+        // 获取当前选项卡导航li的自定义属性index
+        var index = this.getAttribute("index");
 
-		// 先隐藏所有选项卡项目
-		for(var itemi = 0; itemi < btn_item_li_list.length; itemi++) {
-			btn_item_li_list[itemi].style.display = "none";
-		}
+        // 先隐藏所有选项卡项目
+        for (var itemi = 0; itemi < btn_item_li_list.length; itemi++) {
+            btn_item_li_list[itemi].style.display = "none";
+        }
 
-		// 单机选项卡导航时显示
-		btn_item_li_list[index].style.display = "block";
-	}
+        // 单机选项卡导航时显示
+        btn_item_li_list[index].style.display = "block";
+    }
 }
