11729a9bb00b91e55fa9e03da5c13fb02dac8426
liusxs <1694181697@qq.com>
2023-02-27 13:32:00 +0800

    Update App.vue
    页面bug 已经修复完成

--- MobileCommerce/src/App.vue
+++ MobileCommerce/src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <router-link to="/head">head</router-link>
+    <router-link to="/boutique">boutique</router-link>
+    <router-link to="/recommend">recommend</router-link>
+
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
