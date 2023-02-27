06f421af26a78c6227d0e701fb60880ff802491a
liusxs <1694181697@qq.com>
2023-02-27 15:14:59 +0800

    头部更新完成
    暂时缺少图标 等待后续添加

--- MobileCommerce/src/App.vue
+++ MobileCommerce/src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div id="app">
-    <router-link to="/head">head</router-link>
-    <router-link to="/boutique">boutique</router-link>
-    <router-link to="/recommend">recommend</router-link>
+    <Head></Head>
+    <Footer></Footer>
+    <Recommend></Recommend>
+    <boutique></boutique>
 
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
+import Head from './components/Head.vue'
+import Footer from './components/Footer.vue'
+import Recommend from './views/Recommend.vue'
+import Boutique from './views/Boutique.vue'
 export default {
   name: "app",
+  components:{
+    Head,
+    Footer,
+    Recommend,
+    Boutique
+  }
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
 </style>
--- MobileCommerce/src/components/Head.vue
+++ MobileCommerce/src/components/Head.vue
@@ -1,10 +1,42 @@
 <template>
-    <h1>我是twwo</h1>
+  <div class="head">
+    <input type="text" placeholder="搜索商品/店铺" />
+    <div class="icon"></div>
+  </div>
 </template>
 <script>
-export default{
-    data(){
-        return{}
-    }
-}
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
+<style lang="less" scoped>
+.head {
+  height: 96px;
+  background-color: rgb(246, 86, 86);
+  /** 弹性布局 **/
+  display: flex;
+  justify-content: center;
+  /** 垂直居中      使用align-items或align-content的属性，就可以做到垂直居中的效果 **/
+  align-items: center;
+}
+input {
+  height: 60px;
+  font-size: 24px;
+  font-weight: bold;
+  padding: 10px;
+  border: 0;
+  border-radius: 10px;
+  &:focus {
+    outline: none;
+  }
+}
+.icon {
+  height: 60px;
+  width: 60px;
+  background-color: rgb(12, 52, 227);
+  z-index: 666;
+  margin-left: 15px;
+}
+</style>
