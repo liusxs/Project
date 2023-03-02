c168228c877af441993313b8c36b412f5b36cf58
liusxs <1694181697@qq.com>
2023-03-02 13:55:54 +0800

    update

--- MobileCommerce/src/main.js
+++ MobileCommerce/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 new Vue({
   el: '#app',
-  router,
+  // router, 
   render: h => h(App)
 })
