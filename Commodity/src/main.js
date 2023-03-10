cb6712e53bda638ea24c796f22d74a97d0c904c8
liusxs <1694181697@qq.com>
2023-03-10 09:14:46 +0800

    project config

--- Commodity/src/main.js
+++ Commodity/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ELementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ELementUI);
 
 Vue.config.productionTip = false
 
--- Commodity/vue.config.js
+++ Commodity/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //项目编译自动打开的配置
+  devServer:{
+    open:true,
+    host:'localhost'
+  }
 })
