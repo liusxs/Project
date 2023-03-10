0dd9927fdd11236e52221b100afe428fa9f9ce04
liusxs <1694181697@qq.com>
2023-03-10 09:23:05 +0800

    Update main.js
    添加注释  更易于了解

--- Commodity/src/main.js
+++ Commodity/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+//全局引入element-ui组件
 import ELementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ELementUI);
 
+//阻止显示生成模式的消息
 Vue.config.productionTip = false
 
 new Vue({
+  // h 是 Vue.js 里面的 createElement 函数，这个函数的作用就是生成一个 VNode节点，render 函数得到这个 VNode 节点之后，返回给 Vue.js 的 mount 函数，渲染成真实 DOM 节点，并挂载到根节点上
   render: h => h(App),
 }).$mount('#app')
