dbf64019e2dffc597c286a0e0811a5ed7eecdbb8
liusxs <1694181697@qq.com>
2023-03-30 15:20:20 +0800

    更新
    增加全局导航栏，优化布局

--- meituans/src/view/home/Home.vue
+++ meituans/src/view/home/Home.vue
@@ -1,31 +1,13 @@
 <template>
-    <div class="home">
-        <div class="content"></div>
-        <Footer />
+    <div>
+        <span>首页</span>
     </div>
 </template>
     
 <script>
-import Footer from '@/components/Footer.vue';
-export default {
-    components: {
-        Footer,
-    }
-}
+
 </script>
     
-<style lang="less" scoped>
-.home {
-    display: flex;
-    flex-flow: column;
-    height: 100%;
-    .content{
-        flex: 1;
-        /*
-        设置当内容溢出块级元素的上下两侧时所显示的内容
-        取决于浏览器本身。当内容发生溢出时，桌面浏览器如 Firefox 会显示滚动条
-        */
-        overflow-y: auto;
-    }
-}
+<style>
+
 </style>    
