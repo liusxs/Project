33d7b70dc6cfa8c3c5832acb13f6ad1bb14d5afd
liusxs <1694181697@qq.com>
2023-04-10 15:22:23 +0800

    Update Order.vue
    更新订单页面 增加头部导航栏

--- meituans/src/view/order/Order.vue
+++ meituans/src/view/order/Order.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="order">
-        <div class="content">订单</div>
+        <Header title="订单" />
+        <div class="content">
+            <van-tabs color="#ffc400">
+                <van-tab :title="item" v-for="(item, index) in navData">内容 1</van-tab>
+            </van-tabs>
+        </div>
         <Footer />
     </div>
 </template>
     
 <script>
 import Footer from '@/components/Footer.vue';
+import Header from '@/components/Header.vue';
+import { reactive, toRefs } from 'vue';
 export default {
     components: {
         Footer,
+        Header,
     },
+    setup() {
+        const data = reactive({
+            navData: ['全部', '交易完成', '待付款', '待发货', '已发货']
+        })
+        return {
+            ...toRefs(data)
+        }
+    }
 }
 </script>
     
