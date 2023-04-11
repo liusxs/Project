aeb3a99abe70a8431c866639b94aa1e556f703fc
liusxs <1694181697@qq.com>
2023-04-11 11:47:31 +0800

    Update Address.vue

--- meituans/src/view/address/Address.vue
+++ meituans/src/view/address/Address.vue
@@ -1,19 +1,38 @@
 <template>
-    <div>
-        地址管理
+    <div class="address">
+        <Header title="地址管理" />
+        <van-address-list :list="list" default-tag-text="默认" @add="onAdd" @edit="onEdit" />
     </div>
 </template>
 <script>
 import Header from '@/components/Header.vue';
+import { reactive, toRefs } from 'vue'
 export default {
     components: {
         Header,
     },
-    setup(){
-        
-        return{
-
+    setup() {
+        const onAdd = () => { }
+        const onEdit = () => { }
+        const data = reactive({
+            list:[]
+        })
+        return {
+            onAdd,
+            onEdit,
+            ...toRefs(data),
         }
     }
 }
 </script>
+<style lang="less" scoped>
+/deep/.van-button {
+    background-color: #ffc400;
+    border-color: #ffc400;
+}
+
+/deep/.van-address-list__bottom {
+    border-color: #ffc400;
+    padding: 0;
+}
+</style>
